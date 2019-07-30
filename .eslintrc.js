d65287417ff4f3ac51c02983aee58b31300854cb
TakCastel <takcastel@gmail.com>
2019-07-30 11:44:46 +0200

    Add linter rule for HTML markup

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,14 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
     '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+  
   parserOptions: {
     parser: 'babel-eslint'
   }
